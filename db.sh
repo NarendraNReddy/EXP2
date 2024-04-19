d0a1a8488a9766fc6a4ed0f0c7cdbb7542967210
NarendraReddy <narendra.nya@gmail.com>
2024-04-19 16:48:32 -0700

    db add

--- db.sh
+++ db.sh
@@ -1,15 +1,53 @@
-#!/usr/bin 
-USERID=$(id -u)
-echo $USERID
+#!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+BL="\e[34m"
+N="\e[0m"
+
+echo "Enter password:"
+read DB_SERVER_PASSWORD
+#ExpenseApp@1
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ];
+    then 
+        echo -e "$2 is ... $R FAILURE $N"
+    else 
+        echo -e "$2 is ... $G SUCCESS $N"    
+    fi
+}
+
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ];
 then 
-    echo "Please try the instructions in super user mode"
-    exit 1 
-else
-    echo "Super User"    
-fi
+    echo "Be a super user to install the commands"
+    exit 1
+else 
+    echo "Super User"     
+fi  
+
+dnf install mysql-server -y
+VALIDATE $? "mysql server installation"
+
+
+systemctl enable mysqld
+VALIDATE $? "enable the mysqld"
+
+
+systemctl start mysqld
+VALIDATE $? "start the mysqld"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "setting up username and password for DB"
+
+
 
 
 
